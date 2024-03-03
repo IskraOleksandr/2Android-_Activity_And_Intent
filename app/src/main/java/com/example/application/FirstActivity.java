27cb24390649d498b23a2d0f675765a86e2d9fa2
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2024-03-03 18:27:07 +0200

    Сделано второе задание

---
+++ app/src/main/java/com/example/application/FirstActivity.java
@@ -0,0 +1,27 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Button button1 = findViewById(R.id.SecActbutton);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
